8c9d2abfa5194d9c5319367aa746e6e292f3de24
jmuster <jonathan.muster@redsalud.gob.cl>
2020-08-04 16:22:28 -0400

    se agrego carpeta js con el archivo correspondiente

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function metodo_jalisco(){
+    var numero = document.getElementById("input_jalisco");
+    var total = parseInt(numero.value) + 1
+    alert("y yo digo "+ total +"!!!! Gané!!!");
+}
